7884a5f0e000ec3b010402719544a373613e238a
tassili-crypto <tassili2004@gmail.com>
2025-11-27 12:51:37 +0200

    hhjmkm

---
+++ javaTeamProject/src/javateamproject/Frame.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javateamproject;
+
+/**
+ *
+ * @author نبيهة
+ */
+public class Frame {
+    String s ;
+    
+}
